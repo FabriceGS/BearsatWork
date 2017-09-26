a6d45c580f8ff687bb6fec6fb50472795193c669
Brown Loaner <fabrice_guyot-sionnest@brown.edu>
2017-09-26 10:16:14 -0300

    Changed package.json

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express'); // Express web server framework
 const bodyParser = require("body-parser");
-var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
@@ -136,27 +135,9 @@ function getNewToken(oauth2Client, callback) {
         return;
       }
       oauth2Client.credentials = token;
-      storeToken(token);
       callback(oauth2Client);
     });
   });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
 }
 
 function setOauth2Client(client) {
